b72914d193c9899b06899a4815c1cdb8f2a85285
Antonio <afijog@gmail.com>
2013-07-05 21:20:50 +0200

    index.html file added

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end(index);
 });
 
 var port = process.env.PORT || 5000;
