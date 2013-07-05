95d829ba26305c817143ad21a1083b32c3ef563a
Antonio <afijog@gmail.com>
2013-07-05 21:39:46 +0200

    Basic HTML5 layout + bootstrap

--- web.js
+++ web.js
@@ -2,10 +2,20 @@ var express = require('express');
 var fs = require('fs');
 var index = fs.readFileSync('index.html');
 
-var app = express.createServer(express.logger());
+var app = express.createServer();
+
+// configure Express
+app.configure(function() {
+  app.use(express.logger());
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/static'));
+});
 
 app.get('/', function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.writeHead(200, {'Content-Type': 'text/html'});
   response.end(index);
 });
 
